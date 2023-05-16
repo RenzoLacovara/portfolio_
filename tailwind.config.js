91cb92c5ff23d78fa9e0d14b700ac5afde3d36b6
Renzo Lacovara <rnlacovara@gmail.com>
2023-05-16 12:35:31 -0300

    details

--- tailwind.config.js
+++ tailwind.config.js
@@ -188,9 +188,9 @@ module.exports = {
         custom: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
       },
       backgroundImage: {
-        clouds: 'url(../../public/images/Cloudy.svg)',
-        dots: 'url(../../public/images/dots.svg)',
-        waves: 'url(../../public/images/wave.svg)',
+        clouds: 'url(/../public/images/Cloudy.svg)',
+        dots: 'url(../public/images/dots.svg)',
+        waves: 'url(/public/images/wave.svg)',
         waveup: 'url(../../public/images/wave1.svg)',
         wavedown: 'url(../../public/images/wave2.svg)',
         peak: 'url(../../public/images/peak.svg)',
